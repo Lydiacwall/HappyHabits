4b03428837e1f92e39c648cce51537e0846b1e4b
Lydiacwall <88028518+Lydiacwall@users.noreply.github.com>
2024-04-20 20:01:10 +0300

    new screen

--- Mobile-App/app/src/main/java/com/example/happyhabits/View/MainActivity.kt
+++ Mobile-App/app/src/main/java/com/example/happyhabits/View/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.happyhabits.View
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +19,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -31,6 +35,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -78,23 +83,23 @@ import com.example.happyhabits.ViewModel.MainViewModel
 class MainActivity : ComponentActivity() {
 
 //TODO : Remove the black header
-    val First = FontFamily(
-        Font(R.font.oi_regular, FontWeight.Bold),
-    )
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            val viewModel= ViewModelProvider(this)[MainViewModel::class.java]
 
-            setContent {
-                LogoView()
-            }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        setContent {
+           ChooseRole()
         }
+    }
 
 
-
-    @Preview
+    //@Preview
     @Composable
-    fun LogoView(){
+    fun LogoView() {
+        val first = FontFamily(
+            Font(R.font.oi_regular, FontWeight.Bold),
+        )
         val colors = listOf(Color(0xffA586FD), Color(0xff64519A), Color(0xff645199))
         val letter_color = Color(0xff3A2F59)
         Box(
@@ -115,13 +120,13 @@ class MainActivity : ComponentActivity() {
                     .padding(top = 100.dp),
                 style = TextStyle(
                     fontWeight = FontWeight.Bold,
-                    fontFamily = First,
+                    fontFamily = first,
                     lineHeight = 50.sp
                 )
             )
 
             Image(
-                painter= painterResource (R.drawable.logoscreen_icon),
+                painter = painterResource(R.drawable.logoscreen_icon),
                 contentDescription = null,
                 contentScale = ContentScale.Fit,
                 modifier = Modifier
@@ -131,7 +136,8 @@ class MainActivity : ComponentActivity() {
 
             )
 
-            Text(text = "Keep track of what is important to you!",
+            Text(
+                text = "Keep track of what is important to you!",
                 color = Color.White,
                 fontWeight = FontWeight.SemiBold,
                 fontSize = 20.sp,
@@ -142,16 +148,9 @@ class MainActivity : ComponentActivity() {
                     .padding(bottom = 60.dp)
 
             )
-
-//        Box(
-//            modifier = Modifier
-//                .fillMaxWidth() // Make the Box fill the width to allow centering within it
-//                .padding(bottom = 32.dp), // Adjust the padding as needed
-//            contentAlignment = Alignment.BottomCenter // Align the content of the Box to the bottom center
-//        ) {
             Button(
                 onClick = {
-                          viewModel.Na
+                    //viewModel.Na
                 },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color(0xff8A6AE5),
@@ -160,7 +159,7 @@ class MainActivity : ComponentActivity() {
                 shape = RoundedCornerShape(10.dp),
                 contentPadding = PaddingValues(
                     vertical = 15.dp, // The padding inside the button, vertical
-                    horizontal =90.dp // The padding inside the button, horizontal
+                    horizontal = 90.dp // The padding inside the button, horizontal
                 ),
                 elevation = ButtonDefaults.buttonElevation(
                     defaultElevation = 20.dp,
@@ -173,7 +172,7 @@ class MainActivity : ComponentActivity() {
             ) {
                 Text(
                     text = "Get Started",
-                    fontSize =25.sp,
+                    fontSize = 25.sp,
                     fontWeight = FontWeight.Bold
                 )
             }
@@ -182,10 +181,10 @@ class MainActivity : ComponentActivity() {
 
     }
 
-    @Preview
+    //@Preview
     @Composable
-    fun CreateSignInView(){
-        val colors = listOf(Color(0xffF8F7FA),Color(0xffA687FF))
+    fun CreateSignInView() {
+        val colors = listOf(Color(0xffF8F7FA), Color(0xffA687FF))
         var emailInput by remember {
             mutableStateOf("")
         }
@@ -242,16 +241,18 @@ class MainActivity : ComponentActivity() {
                     .fillMaxHeight(0.8f)
             )
             {
-                Text(text = "Hello!",
-                    color= Color.Black,
+                Text(
+                    text = "Hello!",
+                    color = Color.Black,
                     fontSize = 40.sp,
                     style = TextStyle(
                         fontWeight = FontWeight.Bold,
                         lineHeight = 30.sp
                     )
                 )
-                Text(text = "Sign into your account",
-                    color= Color.Black,
+                Text(
+                    text = "Sign into your account",
+                    color = Color.Black,
                     fontSize = 30.sp,
                     style = TextStyle(
                         fontWeight = FontWeight.Bold,
@@ -259,8 +260,9 @@ class MainActivity : ComponentActivity() {
                     )
                 )
                 Spacer(modifier = Modifier.height(30.dp))
-                Text(text = "E M A I L",
-                    color= Color.Black,
+                Text(
+                    text = "E M A I L",
+                    color = Color.Black,
                     fontSize = 15.sp,
                     style = TextStyle(
                         fontWeight = FontWeight.Bold
@@ -285,8 +287,9 @@ class MainActivity : ComponentActivity() {
 
 
                 Spacer(modifier = Modifier.height(16.dp))
-                Text(text = "P A S S W O R D",
-                    color= Color.Black,
+                Text(
+                    text = "P A S S W O R D",
+                    color = Color.Black,
                     fontSize = 15.sp,
                     style = TextStyle(
                         fontWeight = FontWeight.Bold
@@ -321,11 +324,12 @@ class MainActivity : ComponentActivity() {
                         .fillMaxWidth()
                         .height(50.dp)
                         .background(Color(0xff8A6AE5))
-                        .shadow(4.dp))
+                        .shadow(4.dp)
+                )
                 {
                     Text(
                         text = "Login",
-                        fontSize =25.sp,
+                        fontSize = 25.sp,
                         fontWeight = FontWeight.Bold
                     )
                 }
@@ -333,13 +337,13 @@ class MainActivity : ComponentActivity() {
                 Text(
                     text = "Not a member?",
                     color = Color.White,
-                    fontSize =25.sp,
+                    fontSize = 25.sp,
                     fontWeight = FontWeight.Bold
                 )
                 Text(
                     text = "Sign Up",
                     color = Color(0xff8A6AE5),
-                    fontSize =27.sp,
+                    fontSize = 27.sp,
                     fontWeight = FontWeight.ExtraBold
                 )
             }
@@ -347,22 +351,196 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @Preview
+    @Composable
+    fun ChooseRole() {
+        var selectedRole by remember{ mutableStateOf<Int?>(null) }
+        val font = FontFamily(
+            Font(R.font.josefinsans_bold, FontWeight.Bold),
+        )
+        val colors = listOf(Color(0xffA586FD), Color(0xff64519A), Color(0xff645199))
+        var alpha1= 1f
+        var alpha2=1f
+        var elevation1=10.0f
+        var elevation2=10.0f
+
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(colors = colors)
+                )
+
+        )
+        {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .align(Alignment.TopCenter),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Choose Your Role",
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    fontFamily = font,
+                    modifier = Modifier
+                        .padding(top=100.dp)
+
+                    )
+                Spacer(modifier = Modifier.height(10.dp))
+                //-----------------SIMPLE USER-----------------------------
+                Row(
+
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { selectedRole = 1 }
+                        .graphicsLayer {
+                            this.alpha = alpha1
+                            this.shadowElevation = elevation1
+                            this.clip = true
+                        }
+                )
+                {
+                    Text(
+                        text = "SIMPLE USER",
+                        color = Color.White,
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Medium,
+                        fontFamily = font,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(start = 30.dp)
+                    )
+                    Spacer(modifier = Modifier.width(16.dp)) // Adjust spacing as necessary
+                    Image(
+                        painter = painterResource(R.drawable.simple_user_photo),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier.size(250.dp)
+                    )
+
+
+                }
+
+                Spacer(modifier = Modifier.height(30.dp))
+                // -------------------- DOCTOR -------------------------
+                Row(
+
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { selectedRole = 2 }
+                        .graphicsLayer {
+                            this.alpha = alpha2
+                            this.shadowElevation = elevation2
+                            this.clip = true
+                        }
+                )
+                {
+                    Image(
+                        painter = painterResource(R.drawable.doctor_photo),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier
+                            .size(250.dp)
+                            .padding(start = 7.dp)
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = "DOCTOR",
+                        color = Color.White,
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Medium,
+                        fontFamily = font,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(bottom = 12.dp)
+                    )
+
+
+                }
+
+//                Box() {
+//                    Button(
+//                        onClick = {},//TODO : go to the next screen
+//                        shape = RoundedCornerShape(10),
+//                        modifier = Modifier
+//                            .align(Alignment.BottomCenter)
+//                            .padding(bottom = 100.dp)
+//                            .width(300.dp)
+//                            .height(48.dp),
+//
+//
+//                        colors = ButtonDefaults.buttonColors(
+//                            containerColor = Color(0xFFF8E1FB)
+//
+//                        )
+//
+//                    )
+//                    {
+//                        Text(
+//                            text="Continue",
+//                            color= Color.Black,
+//                            fontSize = 24.sp
+//                        )
+//                    }
+//
+//                }
 
 
 
 
+            }
+        }
+
+        if(selectedRole!= null) {
+
+            Box() {
+                Button(
+                    onClick = {},//TODO : go to the next screen
+                    shape = RoundedCornerShape(10),
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(bottom = 100.dp)
+                        .width(300.dp)
+                        .height(48.dp),
 
 
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFFF8E1FB)
 
+                    )
 
+                )
+                {
+                    Text(
+                        text="Continue",
+                        color= Color.Black,
+                        fontSize = 24.sp
+                    )
+                }
 
-
-
-
-
-
+            }
+            if(selectedRole==1){
+                elevation2=0.0f
+                alpha2=0.3f
+            }
+            else if(selectedRole==2){
+                elevation1=0.0f
+                alpha1=0.3f
+            }
+        }
+    }
+}
+private fun selectedRole(role : Int){
 
 }
+
+
 
 
 
